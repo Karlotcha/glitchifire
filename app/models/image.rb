8b58f95f2f2a62c9742de73d7dff9788c130897f
Karlo <karlo.hoa@gmail.com>
2016-03-20 20:22:34 +0000

    trying to mess up with base64

--- app/models/image.rb
+++ app/models/image.rb
@@ -9,8 +9,11 @@ class Image
   def initialize(file = nil)
     return unless file
 
+    @file         = file
     @content_type = file.content_type
-    @content = file.read
+    @content      = file.read
+
+    glitchify!
   end
 
   def base64
@@ -20,5 +23,19 @@ class Image
 
   def persisted?
     false
+  end
+
+  private
+
+  def glitchify!
+    headers = @content.split("\n")[0..4]
+    body = @content.split("\n")[5..-1]
+    new_body = body
+    new_body[0] = Base64.decode64(Base64.encode64(new_body[0]).sub('A', 'AA'))
+    # new_body = body.map { |l| Base64.decode64(Base64.encode64(l).sub('A', 'B')) }
+
+    # binding.pry
+
+    @content =(headers + new_body).join("\n")
   end
 end
